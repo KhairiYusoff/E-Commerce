0293200d76b92d62e9f33e5534f81cace746a0fc
khairi.yusoff <mkhairiyusoff.1985@gmail.com>
2024-05-10 19:40:42 +0800

    style(Announcement): Add moving text animation effect

--- client/src/components/Announcement.js
+++ client/src/components/Announcement.js
@@ -1,4 +1,14 @@
-import styled from "styled-components";
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const moveAnnouncement = keyframes`
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+`;
 
 const Container = styled.div`
   height: 30px;
@@ -9,10 +19,34 @@ const Container = styled.div`
   justify-content: center;
   font-size: 14px;
   font-weight: 500;
+  overflow: hidden;
+  position: relative;
+
+  &:before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    background: linear-gradient(
+      to right,
+      teal 0%,
+      teal 25%,
+      transparent 50%,
+      teal 75%,
+      teal 100%
+    );
+    animation: ${moveAnnouncement} 10s linear infinite;
+  }
 `;
 
 const Announcement = () => {
-    return <Container>Super Deal! Free Shipping on Orders Over $50</Container>;
+  return (
+    <Container>
+      <span>Super Deal! Free Shipping on Orders Over $50</span>
+    </Container>
+  );
 };
 
 export default Announcement;
