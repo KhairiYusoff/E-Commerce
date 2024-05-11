836d47d68b92c5f83e075eee4fb7a26a50e54027
khairi.yusoff <mkhairiyusoff.1985@gmail.com>
2024-05-11 16:00:05 +0800

    Update data.js to include allCat

--- client/src/data.js
+++ client/src/data.js
@@ -43,6 +43,69 @@ export const categories = [
   },
 ];
 
+export const allCat = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "WOMEN'S WEAR",
+    cat: "women",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1562183241-b937e95585b6?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "WALK IN COMFORT",
+    cat: "shoes",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1585820117003-4d6aafa444ac?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "JEANS & JACKETS",
+    cat: "jeans",
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/flagged/photo-1564468781192-f023d514222d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "MEN'S WEAR",
+    cat: "men",
+  },
+  {
+    id: 5,
+    img: "https://images.unsplash.com/photo-1537832816519-689ad163238b?q=80&w=2059&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "ACCESSORIES",
+    cat: "accessories",
+  },
+  {
+    id: 6,
+    img: "https://images.unsplash.com/photo-1525507119028-ed4c629a60a3?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "KIDS' WEAR",
+    cat: "kids",
+  },
+  {
+    id: 7,
+    img: "https://images.unsplash.com/photo-1492707892479-7bc8d5a4ee93?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "BEAUTY & PERSONAL CARE",
+    cat: "beauty",
+  },
+  {
+    id: 8,
+    img: "https://images.unsplash.com/photo-1602211844066-d3bb556e983b?q=80&w=2013&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "SPORTS & ACTIVEWEAR",
+    cat: "sports",
+  },
+  {
+    id: 9,
+    img: "https://images.unsplash.com/photo-1594026112284-02bb6f3352fe?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "HOME & LIVING",
+    cat: "home",
+  },
+  {
+    id: 10,
+    img: "https://images.unsplash.com/photo-1623305615868-6c1134bb8f00?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "ELECTRONICS",
+    cat: "electronics",
+  },
+];
+
 export const popularProducts = [
   {
     id: 1,
